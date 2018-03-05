959e0b5fcf262f2e7fe29c16651a60e53d909b80
xuecat <1446969391@qq.com>
2018-03-05 14:17:24 +0800

    add contractplugin

--- Applications/XYHContractPlugin/Controllers/ContractInfoController.cs
+++ Applications/XYHContractPlugin/Controllers/ContractInfoController.cs
@@ -4,26 +4,47 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using AspNet.Security.OAuth.Validation;
 using ApplicationCore;
 using ApplicationCore.Dto;
 using ApplicationCore.Filters;
 using XYH.Core.Log;
 
-
-namespace XYHHumanPlugin.Controllers
+namespace XYHContractPlugin.Controllers
 {
     //[Authorize(AuthenticationSchemes = OAuthValidationDefaults.AuthenticationScheme)]
     [Produces("application/json")]
-    [Route("api/humaninfo")]
-   public class HumanInfoController : Controller
+    [Route("api/contractinfo")]
+    public class ContractInfoController : Controller
     {
-        private readonly ILogger Logger = LoggerManager.GetLogger("Humaninfo");
+        private readonly ILogger Logger = LoggerManager.GetLogger("Contractinfo");
 
 
         [HttpGet("testinfo")]
         [TypeFilter(typeof(CheckPermission), Arguments = new object[] { "" })]
         public async Task<ResponseMessage<List<int>>> GetTestInfo()//[FromRoute]string testinfo
+        {
+            var Response = new ResponseMessage<List<int>>();
+            //if (string.IsNullOrEmpty(testinfo))
+            //{
+            //    Response.Code = ResponseCodeDefines.ModelStateInvalid;
+            //    Response.Message = "请求参数不正确";
+            //}
+            try
+            {
+                //Response.Extension = await _userTypeValueManager.FindByTypeAsync(user.Id, type, HttpContext.RequestAborted);
+            }
+            catch (Exception e)
+            {
+                Response.Code = ResponseCodeDefines.ServiceError;
+                Response.Message = "服务器错误：" + e.ToString();
+                Logger.Error("error");
+            }
+            return Response;
+        }
+
+        [HttpGet("{contractid}")]
+        [TypeFilter(typeof(CheckPermission), Arguments = new object[] { "" })]
+        public async Task<ResponseMessage<List<int>>> GetContractByid(UserInfo user, [FromRoute] string contractId)
         {
             var Response = new ResponseMessage<List<int>>();
             //if (string.IsNullOrEmpty(testinfo))
---
+++ Applications/XYHContractPlugin/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using ApplicationCore;
+using ApplicationCore.Plugin;
+
+
+namespace XYHContractPlugin
+{
+    public class Plugin : PluginBase
+    {
+        public override string PluginID
+        {
+            get
+            {
+                return "7f5eebef-82a4-4493-ba9c-b255b30120b4";
+            }
+        }
+
+        public override string PluginName
+        {
+            get
+            {
+                return "合同数据";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "合同数据插件";
+            }
+        }
+
+
+        public override Task<ResponseMessage> Init(ApplicationContext context)
+        {
+            //context.Services.AddDbContext<BaseDataDbContext>(options => options.UseMySql("Server=server-d01;database=xinyaohang;uid=root;pwd=root;"));
+            //context.Services.AddDbContext<TestContext>(opt => opt.UseMemoryCache("ToDoList"));
+            return base.Init(context);
+        }
+
+
+        public override Task<ResponseMessage> Start(ApplicationContext context)
+        {
+            return base.Start(context);
+        }
+
+        public override Task<ResponseMessage> Stop(ApplicationContext context)
+        {
+            return base.Stop(context);
+        }
+    }
+}
