15f74ae1777e7a9994c366bb103dba18202e3ef2
heyang <heyangmail@126.com>
2018-04-11 14:47:55 +0800

    添加接口的map

---
+++ Applications/XYHContractInterfacePlugin/ContractInterfaceMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace XYHContractInterfacePlugin
+{
+    public class ContractInterfaceMappingProfile : Profile
+    {
+        public ContractInterfaceMappingProfile()
+        {
+            CreateMap<XYHContractPlugin.Dto.Response.ExamineResponse, GatewayInterface.Dto.ExamineResponse>();
+            CreateMap<GatewayInterface.Dto.ExamineSubmitRequest, XYHContractPlugin.Dto.Response.ExamineResponse>();
+
+            CreateMap<ApplicationCore.Dto.UserInfo, GatewayInterface.Dto.UserInfo>();
+            CreateMap<GatewayInterface.Dto.UserInfo, ApplicationCore.Dto.UserInfo>();
+
+            CreateMap<ApplicationCore.ResponseMessage, GatewayInterface.Dto.ResponseMessage>();
+            CreateMap<GatewayInterface.Dto.ResponseMessage, ApplicationCore.ResponseMessage>();
+
+            CreateMap<ApplicationCore.ResponseMessage<bool>, GatewayInterface.Dto.ResponseMessage<bool>>();
+            CreateMap<GatewayInterface.Dto.ResponseMessage<bool>, ApplicationCore.ResponseMessage<bool>>();
+        }
+    }
+}
