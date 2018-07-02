ede2a670d4555641a76d0edf2e4315356621eced
stone9159 <1515027654@qq.com>
2018-07-03 00:06:01 +0800

    黑名单

--- DesktopPortal/src/humanSystem/pages/black/addblack.js
+++ DesktopPortal/src/humanSystem/pages/black/addblack.js
@@ -1,21 +1,23 @@
-import {connect} from 'react-redux';
-import {getDicParList, postBlackLst} from '../../actions/actionCreator';
-import React, {Component} from 'react'
-import {Table, Input, Select, Form, Button, Row, Col, Checkbox, Pagination, Spin} from 'antd'
+import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { Table, Input, Select, Form, Button, Row, Col, Checkbox, Pagination, Spin, notification } from 'antd'
 import Layer from '../../../components/Layer'
 import BlackForm from '../../../businessComponents/humanSystem/blackInfo'
-import {NewGuid} from '../../../utils/appUtils';
+import { NewGuid } from '../../../utils/appUtils';
+import ApiClient from '../../../utils/apiClient'
+import WebApiConfig from '../../constants/webapiConfig';
 const FormItem = Form.Item;
 const ButtonGroup = Button.Group;
 const formItemLayout1 = {
-    labelCol: {span: 6},
-    wrapperCol: {span: 6},
+    labelCol: { span: 6 },
+    wrapperCol: { span: 6 },
 };
 
 
 class Black extends Component {
     state = {
-        blackForm: null
+        blackForm: null,
+        showLoading: false
     }
     componentWillMount() {
     }
@@ -23,31 +25,58 @@ class Black extends Component {
     componentDidMount() {
 
     }
+    //提交黑名单
+    submitBlack(entity, type) {
+        this.setState({ showLoading: true });
+        let url = WebApiConfig.server.SetBlack;
+        let huResult = { isOk: false, msg: '黑名单保存失败！' };
+        try {
+            let res = ApiClient.post(url, entity);
+            this.setState({ showLoading: false });
+            //弹消息，返回
+            if (res.data.code == 0) {
+                huResult.isOk = true;
+                huResult.msg = '黑名单保存成功';
+            }
+        } catch (e) {
+            huResult.msg = "黑名单接口调用异常!";
+        }
+        notification[huResult.isOk ? 'success' : 'error']({
+            message: huResult.msg,
+            duration: 3
+        });
+    }
 
     //子页面回调
     subPageLoadCallback = (formObj, pageName) => {
         console.log("表单对象:", formObj, pageName);
         if (pageName == "black") {
-            this.setState({blackForm: formObj});
+            this.setState({ blackForm: formObj });
         }
     }
     handleSubmit = (e) => {
+        let blackInfo = this.props.location.state;
         e.preventDefault();
         this.state.blackForm.validateFields((err, values) => {
             if (!err) {
-                values.id = NewGuid();
+                if (blackInfo.id) {
+                    values.id = blackInfo.id;
+                } else {
+                    values.id = NewGuid();
+                }
                 console.log("提交的黑名单信息:", values);
-                this.props.dispatch(postBlackLst(values));
+                this.submitBlack(values);
             }
         });
     }
 
     render() {
+        let blackInfo = this.props.location.state;
         return (
-            <Layer className="content-page">
-                <BlackForm subPageLoadCallback={(formObj, pageName) => this.subPageLoadCallback(formObj, pageName)} />
+            <Layer className="content-page" showLoading={this.state.showLoading}>
+                <BlackForm subPageLoadCallback={(formObj, pageName) => this.subPageLoadCallback(formObj, pageName)} entityInfo={blackInfo} />
                 <Row>
-                    <Col style={{textAlign: 'center'}} span={21}>
+                    <Col style={{ textAlign: 'center' }} span={21}>
                         <Button type="primary" onClick={this.handleSubmit} >提交</Button>
                     </Col>
                 </Row>
--- DesktopPortal/src/humanSystem/pages/black/black.js
+++ DesktopPortal/src/humanSystem/pages/black/black.js
@@ -1,15 +1,15 @@
-import {connect} from 'react-redux';
-import {setLoadingVisible, setSearchLoadingVisible, adduserPage, deleteBlackInfo, getBlackList, selBlackList} from '../../actions/actionCreator';
-import React, {Component} from 'react';
-import {Input, Spin, Checkbox, Button, notification, Row, Col, Table} from 'antd';
+import { connect } from 'react-redux';
+import { setLoadingVisible, setSearchLoadingVisible, adduserPage, deleteBlackInfo, getBlackList, selBlackList } from '../../actions/actionCreator';
+import React, { Component } from 'react';
+import { Input, Spin, Popconfirm, Button, notification, Row, Col, Table } from 'antd';
 import './search.less';
-import Layer, {LayerRouter} from '../../../components/Layer'
-import {Route} from 'react-router'
+import Layer, { LayerRouter } from '../../../components/Layer'
+import { Route } from 'react-router'
 import Addblack from './addblack'
 const buttonDef = [
-    {buttonID: "addnew", buttonName: "新建", icon: '', type: 'primary', size: 'large', },
-    {buttonID: "modify", buttonName: "修改", icon: '', type: 'primary', size: 'large', },
-    {buttonID: "delete", buttonName: "删除", icon: '', type: 'primary', size: 'large', },
+    { buttonID: "addnew", buttonName: "新建", icon: '', type: 'primary', size: 'large', },
+    { buttonID: "modify", buttonName: "修改", icon: '', type: 'primary', size: 'large', },
+    { buttonID: "delete", buttonName: "删除", icon: '', type: 'primary', size: 'large', },
 ];
 
 
@@ -20,6 +20,7 @@ class MainIndex extends Component {
             pageIndex: 0,
             pageSize: 0
         },
+        checkList: [],//选中列表
         editblackInfo: null//当前编辑黑名单
     }
 
@@ -28,27 +29,27 @@ class MainIndex extends Component {
     }
     _getColumns() {
         const columns = [
-            {title: '身份证', dataIndex: 'idCard', key: 'idCard', },
-            {title: '姓名', dataIndex: 'name', key: 'name'},
-            {title: '性别', dataIndex: 'sex', key: 'sex', render: (text, record) => <div>{text == 1 ? "男" : '女'}</div>},
-            {title: '电话', dataIndex: 'phone', key: 'phone'},
-            {title: 'email', dataIndex: 'email', key: 'email'},
+            { title: '身份证', dataIndex: 'idCard', key: 'idCard', },
+            { title: '姓名', dataIndex: 'name', key: 'name' },
+            { title: '性别', dataIndex: 'sex', key: 'sex', render: (text, record) => <div>{text == 1 ? "男" : '女'}</div> },
+            { title: '电话', dataIndex: 'phone', key: 'phone' },
+            { title: 'email', dataIndex: 'email', key: 'email' },
         ];
         return columns;
     }
 
     gotoSubPage = (path, params) => {
-        this.props.history.push(`${this.props.match.url}/${path}`, {...params})
+        this.props.history.push(`${this.props.match.url}/${path}`, params || {})
     }
 
     handleClickFucButton = (e) => {
         if (e.target.id === "addnew") {
-            this.props.dispatch(adduserPage({id: 11, menuID: 'menu_blackaddnew', displayName: '新建黑名单', type: 'item'}));
-            this.gotoSubPage('addblack', {ismodify: "0"})
+            this.props.dispatch(adduserPage({ id: 11, menuID: 'menu_blackaddnew', displayName: '新建黑名单', type: 'item' }));
+            this.gotoSubPage('addblack', {})
         } else if (e.target.id === "modify") {
-            if (this.props.selBlacklist.length > 0) {
-                this.props.dispatch(adduserPage({id: 12, menuID: 'menu_blackmodify', displayName: '修改黑名单', type: 'item'}));
-                this.gotoSubPage('addblack', {ismodify: "1"})
+            if (this.state.checkList.length > 0) {
+                this.props.dispatch(adduserPage({ id: 12, menuID: 'menu_blackmodify', displayName: '修改黑名单', type: 'item' }));
+                this.gotoSubPage('editblack', this.state.checkList[0])
             }
             else {
                 notification.error({
@@ -57,9 +58,8 @@ class MainIndex extends Component {
                 });
             }
         } else if (e.target.id === "delete") {
-            if (this.props.selBlacklist.length > 0) {
-                this.props.dispatch(deleteBlackInfo(this.props.selBlacklist[this.props.selBlacklist.length - 1]));
-                //this.props.dispatch(adduserPage({menuID: 'costcharge', disname: '删除黑名单', type:'item'}));//删除要个jb啊
+            if (this.state.checkList.length > 0) {
+                this.props.dispatch(deleteBlackInfo(this.state.checkList[this.state.checkList - 1]));
             }
             else {
                 notification.error({
@@ -92,13 +92,14 @@ class MainIndex extends Component {
     render() {
         const rowSelection = {
             onChange: (selectedRowKeys, selectedRows) => {
-                this.props.dispatch(selBlackList(selectedRows));
+                console.log("选中行:", selectedRows);
+                this.setState({ checkList: selectedRows });
             }
         };
         let showLoading = this.props.showLoading;
         let columns = this._getColumns();
         let blackList = this.props.searchInfoResult.blackList;
-        let paginationInfo = {current: blackList.pageIndex, pageSize: blackList.pageSize, total: blackList.totalCount};
+        let paginationInfo = { current: blackList.pageIndex, pageSize: blackList.pageSize, total: blackList.totalCount };
         return (
             <Layer >
                 {/* <Spin spinning={showLoading}> */}
@@ -114,16 +115,28 @@ class MainIndex extends Component {
                     </Row>
                 </div>
                 {
-                    buttonDef.map(
-                        (button, i) => this.hasPermission(button) ?
-                            <Button key={i} id={button.buttonID} style={{marginTop: '10px', marginBottom: '10px', marginRight: '10px', border: 0}}
-                                onClick={this.handleClickFucButton}
-                                icon={button.icon} size={button.size} type={button.type}>{button.buttonName}</Button> : null
-                    )
+                    buttonDef.map((button, i) => {
+                        let hasPermission = this.hasPermission(button);
+                        if (hasPermission) {
+                            if (button.buttonID == 'delete') {
+                                return (this.state.checkList.length > 0 ? <Popconfirm title="确认要删除黑名单?" onConfirm={this.handleClickFucButton} okText="是" cancelText="否">
+                                    <Button key={i} id={button.buttonID} style={{ marginTop: '10px', marginBottom: '10px', marginRight: '10px', border: 0 }}
+                                        icon={button.icon} size={button.size} type={button.type}>{button.buttonName}</Button>
+                                </Popconfirm> : <Button key={i} id={button.buttonID} style={{ marginTop: '10px', marginBottom: '10px', marginRight: '10px', border: 0 }}
+                                    onClick={this.handleClickFucButton}
+                                    icon={button.icon} size={button.size} type={button.type}>{button.buttonName}</Button>
+                                )
+                            } else {
+                                return (<Button key={i} id={button.buttonID} style={{ marginTop: '10px', marginBottom: '10px', marginRight: '10px', border: 0 }}
+                                    onClick={this.handleClickFucButton}
+                                    icon={button.icon} size={button.size} type={button.type}>{button.buttonName}</Button>)
+                            }
+                        }
+                    })
                 }
                 {/* <SearchResult /> */}
                 <div>
-                    {<p style={{marginBottom: '10px'}}>目前已为你筛选出<b>{this.props.searchInfoResult.blackList.extension.length}</b>条费用信息</p>}
+                    {<p style={{ marginBottom: '10px' }}>目前已为你筛选出<b>{this.props.searchInfoResult.blackList.extension.length}</b>条费用信息</p>}
                     <div id="searchResult">
                         <Table id={"table"} rowKey={record => record.key}
                             columns={columns}
@@ -136,7 +149,7 @@ class MainIndex extends Component {
                 {/* </Spin> */}
                 <LayerRouter>
                     <Route path={`${this.props.match.url}/addblack`} render={(props) => <Addblack  {...props} />} />
-                    <Route path={`${this.props.match.url}/editblack`} render={(props) => <Addblack  {...props} isEdit />} />
+                    <Route path={`${this.props.match.url}/editblack`} render={(props) => <Addblack  {...props} />} />
                 </LayerRouter>
             </Layer>
         )
